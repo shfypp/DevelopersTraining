217b54d530cfc9743b50da8a692e82b6680b457b
shfy <229651638@qq.com>
2016-05-10 10:41:40 +0800

    2 Building Your First App

--- app/src/main/java/cn/hophin/shfy/developerstraining/MainActivity.java
+++ app/src/main/java/cn/hophin/shfy/developerstraining/MainActivity.java
@@ -1,15 +1,19 @@
 package cn.hophin.shfy.developerstraining;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "cn.hophin.shfy.developerstraining.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +52,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Called when the user clicks the Send button
+     */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
